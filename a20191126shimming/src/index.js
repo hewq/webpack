1460759924400d8709e9760e453db874368a775c
wenqiu.he <wenqiu.he@yahoo.com>
2019-11-26 11:37:56 +0800

    update

---
+++ a20191126shimming/src/index.js
@@ -0,0 +1,23 @@
+import 'babel-polyfill';
+
+function component() {
+	var element = document.createElement('div');
+
+	// element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.innerHTML = join(['Hello', 'webpack'], ' ');
+
+	// Assume we are in the context of `window`
+	this.alert('Hmmm, this probably isn\'t a great idea...')
+
+	return element;
+}
+
+fetch('https://jsonplaceholder.typicode.com/users')
+	.then(response => response.json())
+	.then(json => {
+		console.log('We retrieved some data! AND we\'re confident it will work on a variety of browser distributions.')
+		console.log(json)
+	})
+	.catch(error => console.error('Something went wrong when fetching this data: ', error))
+
+document.body.appendChild(component());
